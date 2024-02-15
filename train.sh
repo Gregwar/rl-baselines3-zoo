906e2ad53b7bb91ed64480b35c5c6df279f7591f
Grégoire Passault <g.passault@gmail.com>
2024-02-15 20:12:24 +0100

    Adding tags

--- train.sh
+++ train.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
 
 PROJECT=sigmaban-standup
 ENV=sigmaban-standup-v0
 ALGO=td3
+TAGS=standup
 
 killall -9 python
 
-nohup python train.py --env=$ENV --algo=$ALGO --track --wandb-project-name $PROJECT --vec-env=subproc &
+nohup python train.py --env=$ENV --algo=$ALGO --track --wandb-project-name $PROJECT \
+    --vec-env=subproc --tags $TAGS `hostname` &
 
 tail -f nohup.out
 

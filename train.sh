d3a27df835c402b301d2a8eaae8ced077ae58565
Gregoire Passault <gpassault@emi.u-bordeaux.fr>
2024-02-15 20:14:31 +0100

    Using hostname.out instead of nohup.out

--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@ TAGS=standup
 killall -9 python
 
 nohup python train.py --env=$ENV --algo=$ALGO --track --wandb-project-name $PROJECT \
-    --vec-env=subproc --tags $TAGS `hostname` &
+    --vec-env=subproc --tags $TAGS $HOSTNAME > $HOSTNAME &
 
-tail -f nohup.out
+tail -f $HOSTNAME.out
 

47f7a76fd1e192717a2f702f49b2a3795baed5ab
Gregwar <g.passault@gmail.com>
2024-02-24 23:51:35 +0100

    Evaluating 1000 eps

--- train.sh
+++ train.sh
@@ -12,10 +12,10 @@ TAGS=standup
 EXP_ID=`python next_run_id.py $ALGO $ENV`
 RUN_NAME="${HOSTNAME}_${EXP_ID}_$1"
 
-killall -9 python
+# killall -9 python
 
 nohup python train.py --env=$ENV --algo=$ALGO --track --wandb-project-name $PROJECT \
-    --eval-episodes=100 --n-eval-envs=4 --vec-env=subproc --run-name="$RUN_NAME" \
+    --eval-episodes=1000 --n-eval-envs=4 --vec-env=subproc --run-name="$RUN_NAME" \
      -tags exp-$EXP_ID $TAGS h-$HOSTNAME > $HOSTNAME.out &
 
 tail -f $HOSTNAME.out

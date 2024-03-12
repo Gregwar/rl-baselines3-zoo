974e4d1bb564eb5f28a5f7f0f6b3a0f4fdd2b89b
Gregwar <g.passault@gmail.com>
2024-03-12 16:25:51 +0100

    Run name first in train.sh

--- train.sh
+++ train.sh
@@ -7,15 +7,16 @@ fi
 
 PROJECT=sigmaban-standup
 ENV=sigmaban-standup-v0
-ALGO=td3
+ALGO=sac
 TAGS=standup
 EXP_ID=`python next_run_id.py $ALGO $ENV`
 RUN_NAME="${HOSTNAME}_${EXP_ID}_$1"
 
 # killall -9 python
 
-nohup python train.py --env=$ENV --algo=$ALGO --track --wandb-project-name $PROJECT \
-    --eval-episodes=100 --n-eval-envs=4 --vec-env=subproc --run-name="$RUN_NAME" \
+nohup python train.py --run-name="$RUN_NAME" \
+    --env=$ENV --algo=$ALGO --track --wandb-project-name $PROJECT \
+    --eval-episodes=100 --n-eval-envs=4 --vec-env=subproc \
      -tags exp-$EXP_ID $TAGS h-$HOSTNAME > $HOSTNAME.out &
 
 tail -f $HOSTNAME.out
